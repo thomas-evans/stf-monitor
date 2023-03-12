924f5396200dd2e3b23f0916cde3c62943827d3d
tevans <tce003@gmail.com>
2023-03-12 19:42:50 -0400

    more cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,6 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-import {RouteReuseStrategy} from '@angular/router';
-
-import {IonicModule, IonicRouteStrategy} from '@ionic/angular';
-
+import {IonicModule} from '@ionic/angular';
 import {AppComponent} from './app.component';
 import {DataSetSelectorModule} from "./data-set-selector/data-set-selector.component";
 import {HttpClientModule} from "@angular/common/http";
@@ -12,11 +9,6 @@ import {SeriesDisplayModule} from "./series-display/series-display.component";
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), DataSetSelectorModule, HttpClientModule, SeriesDisplayModule],
-  providers: [
-    {
-      provide: RouteReuseStrategy,
-      useClass: IonicRouteStrategy
-    }],
   bootstrap: [AppComponent]
 })
 export class AppModule {
