b2ddb0ff6ff870a31b429a89fa39f3f57836ccdd
tevans <tce003@gmail.com>
2023-03-19 05:57:01 -0400

    removed puppeteer and added netlify plugin

--- karma.conf.js
+++ karma.conf.js
@@ -1,9 +1,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-process.env.CHROME_BIN = require('puppeteer').executablePath()
-
-
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -40,13 +37,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadlessNoSandbox'],
-    customLaunchers: {
-      ChromeHeadlessNoSandbox: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
-      }
-    },
+    browsers: ['ChromeHeadless'],
     singleRun: false,
     restartOnFileChange: true
   });
